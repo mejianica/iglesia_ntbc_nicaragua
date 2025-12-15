d3e6fae6b229cbce32285bc598b51fea21946a34
Ronaldmera <ronaldalme1230@gmail.com>
2025-12-15 09:51:16 -0500

    imagenes organizadas por carpeta y vista

--- src/assets/sliderImagesHome.js
+++ src/assets/sliderImagesHome.js
@@ -1,6 +1,6 @@
-import img1 from "../assets/slider1.jpg";
-import img2 from "../assets/slider2.jpg";
-import img3 from "../assets/slider3.jpg";
+import img1 from "../assets/images/sliders/home/slider1.jpg";
+import img2 from "../assets/images/sliders/home/slider2.jpg";
+import img3 from "../assets/images/sliders/home/slider3.jpg";
 
 const sliderImagesHome = [img1, img2, img3];
 export default sliderImagesHome;
