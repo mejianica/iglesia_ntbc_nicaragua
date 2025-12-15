2b732206c02a9445c97d2d0c8f36510bc6826099
Ronaldmera <ronaldalme1230@gmail.com>
2025-12-14 19:04:56 -0500

    Navbar, react routes agregado y creacionde  la estructura de carpetas para las rutas y vistas

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
+import AppRoutes from "./routes/AppRoutes";
 
 function App() {
   return (
     <>
       <Navbar />
+      <AppRoutes />
     </>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import Logo from "../assets/LogoNTBC.avif";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-sm bg-primary py-1">
+    <nav className="navbar navbar-expand-md bg-primary py-1">
       <div className="container-fluid">
         {/* LOGO MÓVIL (izquierda) */}
-        <a className="navbar-brand d-sm-none text-white" href="#">
-          <img src={Logo} alt="Logo Iglesia NTBC" /> <span>NTBC Nicaragua</span>
-        </a>
+        <NavLink to="/" className="navbar-brand d-md-none text-white">
+          <img src={Logo} alt="Logo Iglesia NTBC" />
+          <span> NTBC Nicaragua</span>
+        </NavLink>
 
         {/* BOTÓN HAMBURGUESA (derecha en móvil) */}
         <button
-          className="navbar-toggler d-sm-none ms-auto bg-white"
+          className="navbar-toggler d-md-none ms-auto bg-white"
           type="button"
           data-bs-toggle="collapse"
           data-bs-target="#navbarSupportedContent"
@@ -28,51 +30,70 @@ const Navbar = () => {
           className="collapse navbar-collapse justify-content-center"
           id="navbarSupportedContent"
         >
-          {/* LOGO DESKTOP (centrado con links) */}
-          <a
-            className="navbar-brand d-none d-sm-block text-white me-4"
-            href="#"
+          {/* LOGO DESKTOP */}
+          <NavLink
+            to="/"
+            className="navbar-brand d-none d-md-block text-white me-4"
           >
-            <img src={Logo} alt="Logo Iglesia NTBC" />{" "}
-            <span className=" fw-bolder">NTBC Nicaragua</span>
-          </a>
+            <img src={Logo} alt="Logo Iglesia NTBC" />
+            <span className="fw-bolder"> NTBC Nicaragua</span>
+          </NavLink>
 
           {/* MENÚ */}
-          <ul className="navbar-nav mb-2 mb-xl-0">
+          <ul className="navbar-nav mb-2 mb-md-0">
             <li className="nav-item">
-              <a className="nav-link text-white" href="#">
+              <NavLink to="/nosotros" className="nav-link text-white">
                 LIVE
-              </a>
+              </NavLink>
             </li>
-            <li className="nav-item">
-              <a className="nav-link text-white" href="#">
-                ABOUT US
+
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle text-white"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                NOSOTROS
               </a>
+              <ul className="dropdown-menu">
+                <li>
+                  <a className="dropdown-item" href="#">
+                    HOLA
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    PASTOR
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    ORACION
+                  </a>
+                </li>
+              </ul>
             </li>
+
             <li className="nav-item">
-              <a className="nav-link text-white" href="#">
-                ESPAÑOL
-              </a>
+              <NavLink className="nav-link text-white">ESPAÑOL</NavLink>
             </li>
+
             <li className="nav-item">
-              <a className="nav-link text-white" href="#">
-                MINISTRIES
-              </a>
+              <NavLink className="nav-link text-white">MINISTRIES</NavLink>
             </li>
+
             <li className="nav-item">
-              <a className="nav-link text-white" href="#">
-                SCHOOL (DCS)
-              </a>
+              <NavLink className="nav-link text-white">SCHOOL (DCS)</NavLink>
             </li>
+
             <li className="nav-item">
-              <a className="nav-link text-white" href="#">
-                GIVE
-              </a>
+              <NavLink className="nav-link text-white">GIVE</NavLink>
             </li>
+
             <li className="nav-item">
-              <a className="nav-link text-white" href="#">
-                PRAYER
-              </a>
+              <NavLink className="nav-link text-white">PRAYER</NavLink>
             </li>
           </ul>
         </div>
