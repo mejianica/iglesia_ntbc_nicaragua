b5fbbbdd6bea6676f1323abbe9b50368218ed553
Ronaldmera <ronaldalme1230@gmail.com>
2025-12-15 15:20:06 -0500

    nueva imagen en Slider

--- src/assets/sliderImagesHome.js
+++ src/assets/sliderImagesHome.js
@@ -1,6 +1,7 @@
 import img1 from "../assets/images/sliders/home/slider1.jpg";
 import img2 from "../assets/images/sliders/home/slider2.jpg";
 import img3 from "../assets/images/sliders/home/slider3.jpg";
+import img4 from "../assets/images/sliders/home/slider4.jpg";
 
-const sliderImagesHome = [img1, img2, img3];
+const sliderImagesHome = [img4, img2, img3, img1];
 export default sliderImagesHome;
